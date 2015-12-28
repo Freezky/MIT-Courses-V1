6089eb7b335c33eecac8db52b9437ce60badfcf1
Kevyn Bele-Binda <kevynbele-binda@kevyns-macbook-pro.local>
2015-12-27 22:23:03 -0800

    Initial Commit

---
+++ Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by Kevyn Bele-Binda on 15-12-27.
+//  Copyright © 2015 Kevyn Bele-Binda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
